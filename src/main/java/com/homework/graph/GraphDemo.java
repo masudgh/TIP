1db91c322dfeacb22afe4286777b5b2e54f0e14c
masud hassan <masud.prog@gmail.com>
2020-09-11 19:01:16 -0500

    more HW

---
+++ src/main/java/com/homework/graph/GraphDemo.java
@@ -0,0 +1,22 @@
+package com.homework.graph;
+
+/*
+Implement a simple unweighted Graph class with following operations:
++addEdge()
++printUsingBFS()
++printUsingDFS()
++findShortestDistance(src, dst) //return number of hops
++isPathExist(src, dst).  //if path exists between the two vertext
+ */
+
+class SimpleGraph {
+}
+
+
+public class GraphDemo {
+
+
+
+
+
+}
